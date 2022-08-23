14ba43ac2bd3124b7a041da9ebc63db208475930
Suvorov <dew1983@mail.ru>
2022-08-23 23:51:45 +0300

    Добавил PropTypes для IngredientsType

--- src/components/burger-ingredients/ingredients-type/ingredients-type.js
+++ src/components/burger-ingredients/ingredients-type/ingredients-type.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './ingredients-type.module.css';
 import IngredientCard from './ingredient-card/ingredient-card';
 
@@ -24,5 +25,26 @@ const IngredientsType = React.memo(({
     </>
   );
 });
+
+IngredientsType.propTypes = {
+  title: PropTypes.string.isRequired,
+  onShowDetails: PropTypes.func.isRequired,
+  ingredients: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      type: PropTypes.oneOf(['bun', 'main', 'sauce']).isRequired,
+      proteins: PropTypes.number.isRequired,
+      fat: PropTypes.number.isRequired,
+      carbohydrates: PropTypes.number.isRequired,
+      calories: PropTypes.number.isRequired,
+      price: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      image_mobile: PropTypes.string.isRequired,
+      image_large: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
 
 export default IngredientsType;
