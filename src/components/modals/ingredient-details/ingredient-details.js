aba543189ec08fec70407d33ff2d23e1a368e1bd
Suvorov <dew1983@mail.ru>
2022-08-23 21:48:43 +0300

    Исправил стили чисел в окне детализации

--- src/components/modals/ingredient-details/ingredient-details.js
+++ src/components/modals/ingredient-details/ingredient-details.js
@@ -16,19 +16,19 @@ const IngredientDetails = ({
         <div className={styles.info}>
           <div className="text text_type_main-default text_color_inactive">
             <p className="text">Калории,ккал</p>
-            <p className="text">{ingredient.calories}</p>
+            <p className="text text_type_digits-default">{ingredient.calories}</p>
           </div>
           <div className="text text_type_main-default text_color_inactive">
             <p className="text">Белки, г</p>
-            <p className="text">{ingredient.proteins}</p>
+            <p className="text text_type_digits-default">{ingredient.proteins}</p>
           </div>
           <div className="text text_type_main-default text_color_inactive">
             <p className="text">Жиры, г</p>
-            <p className="text">{ingredient.fat}</p>
+            <p className="text text_type_digits-default">{ingredient.fat}</p>
           </div>
           <div className="text text_type_main-default text_color_inactive">
             <p className="text">Углеводы, г</p>
-            <p className="text">{ingredient.carbohydrates}</p>
+            <p className="text text_type_digits-default">{ingredient.carbohydrates}</p>
           </div>
         </div>
       </div>
