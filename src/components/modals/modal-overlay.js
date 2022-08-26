c07989cc5ee00fb7b3a27469cee272dec576e11b
Suvorov <dew1983@mail.ru>
2022-08-26 20:36:17 +0300

    Упростил onClick в ModalOverlay

--- src/components/modals/modal-overlay.js
+++ src/components/modals/modal-overlay.js
@@ -1,16 +1,11 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './modal-overlay.module.css';
 
 const ModalOverlay = ({
   onClose,
 }) => {
-  const onClick = React.useCallback((e) => {
-    onClose();
-  }, [onClose]);
-
   return (
-    <div className={styles.overlay} onClick={onClick}></div>
+    <div className={styles.overlay} onClick={onClose}></div>
   );
 };
 
